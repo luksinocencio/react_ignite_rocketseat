a08043e5ba3948e35cb1ff39fff0a3acd5af2d42
Lucas Inocêncio <luksinocencio@gmail.com>
2024-10-30 16:10:39 -0300

    feat(fundamentos_reactjs): add css global

---
+++ 01_iniciando_com_react/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,17 @@
+import {Post} from "./components/Post";
+import {Header} from "./components/header/Header";
+import "./global.css";
+
+export function App() {
+    return (
+        <div>
+            <Header/>
+            <Post author="Felipe"
+                  content="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."/>
+            <Post author="Gabriel"
+                  content="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et"/>
+        </div>
+    )
+}
+
+
