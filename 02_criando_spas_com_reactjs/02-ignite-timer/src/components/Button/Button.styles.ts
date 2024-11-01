3d7033e7af705188c6b1c7cb3c6993f19984597c
luksinocencio <luksinocencio@gmail.com>
2024-11-01 17:06:54 -0300

    feat(ignite_timer): tipagem de temas

--- 02_criando_spas_com_reactjs/02-ignite-timer/src/components/Button/Button.styles.ts
+++ 02_criando_spas_com_reactjs/02-ignite-timer/src/components/Button/Button.styles.ts
@@ -16,7 +16,6 @@ interface ButtonContainerProps {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  background-color: ${props => props.theme.primary};
   border: 0;
   margin: 8px;
   border-radius: 8px;
