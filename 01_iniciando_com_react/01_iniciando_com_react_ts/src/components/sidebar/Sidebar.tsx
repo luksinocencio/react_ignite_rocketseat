4b4d4090ef3f4c6c954dc95b4cf49ed950be9888
luksinocencio <luksinocencio@gmail.com>
2024-11-01 12:54:14 -0300

    feat(fundamentos_react): add projeto com typescript

---
+++ 01_iniciando_com_react/01_iniciando_com_react_ts/src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,27 @@
+import styles from './Sidebar.module.css';
+import cover from '../../assets/bellagio.jpg';
+import profile from '../../assets/profile.jpeg';
+
+import { PencilLine } from '@phosphor-icons/react';
+import { Avatar } from '../avatar/Avatar';
+
+export function Sidebar() {
+	return (
+		<aside className={styles.sidebar}>
+			<img src={cover} className={styles.cover} alt="Cover"/>
+
+			<div className={styles.profile}>
+				<Avatar src={profile} />
+				<strong>Lucas Inocencio</strong>
+				<span>iOS Developer</span>
+			</div>
+
+			<footer>
+				<a href="#">
+					<PencilLine size={20}/>
+                    Editar seu perfil
+				</a>
+			</footer>
+		</aside>
+	);
+}
---
+++ 01_iniciando_com_react/01_iniciando_com_react_ts/src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import { App } from './App.tsx'
+
+// DOM - Document Object Model
+// Representação do HTML em forma de objetos
+
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<App/>
+	</StrictMode>,
+)
