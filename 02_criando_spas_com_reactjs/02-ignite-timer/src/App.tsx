e16170e291435d819524c3e24553f37654a2f8bc
luksinocencio <luksinocencio@gmail.com>
2024-11-01 17:10:40 -0300

    feat(ignite_timer): add global style

--- 02_criando_spas_com_reactjs/02-ignite-timer/src/App.tsx
+++ 02_criando_spas_com_reactjs/02-ignite-timer/src/App.tsx
@@ -1,6 +1,7 @@
 import { Button } from './components/Button/Button';
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
 	return (
@@ -10,6 +11,7 @@ export function App() {
 			<Button variant="success"/>
 			<Button variant="danger"/>
 			<Button/>
+			<GlobalStyle/>
 		</ThemeProvider>
 	)
 }
