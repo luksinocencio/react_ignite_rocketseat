498a87ee746919b2ab1b194fc1da1556f3add79e
luksinocencio <luksinocencio@gmail.com>
2024-11-01 22:20:43 -0300

    feat(ignite_timer): add react-router-dom

--- 02_criando_spas_com_reactjs/02-ignite-timer/src/App.tsx
+++ 02_criando_spas_com_reactjs/02-ignite-timer/src/App.tsx
@@ -1,12 +1,15 @@
-import { Button } from './components/Button/Button'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" title="Teste" />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
