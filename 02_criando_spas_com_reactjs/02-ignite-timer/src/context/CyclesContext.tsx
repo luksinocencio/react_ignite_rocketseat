317d786ba56a505a5fd629684d3baed30e9c746a
luksinocencio <luksinocencio@gmail.com>
2024-11-06 01:59:45 -0300

    feat(ignite_timer): salvando estado no storage

--- 02_criando_spas_com_reactjs/02-ignite-timer/src/context/CyclesContext.tsx
+++ 02_criando_spas_com_reactjs/02-ignite-timer/src/context/CyclesContext.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, createContext, useReducer, useState } from 'react'
+import { differenceInSeconds } from 'date-fns'
+import { ReactNode, createContext, useEffect, useReducer, useState } from 'react'
 
 import {
   addNewCycleAction,
@@ -30,15 +31,30 @@ interface CyclesContextProviderProps {
 }
 
 export function CyclesContextProvider({ children }: CyclesContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  })
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    (initialState) => {
+      const storedStateJson = localStorage.getItem('@ignite-timer:cycles-state-1.0.0')
+      if (storedStateJson) {
+        return JSON.parse(storedStateJson)
+      }
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
-
+      return initialState
+    },
+  )
   const { cycles, activeCycleId } = cyclesState
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+    }
+    return 0
+  })
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds)
@@ -67,14 +83,19 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
     dispatch(interruptCurrentCycleAction())
   }
 
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+    localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
+  }, [cyclesState])
+
   return (
     <CyclesContext.Provider
       value={{
         cycles,
         activeCycle,
         activeCycleId,
-        markCurrentCycleAsFinished,
         amountSecondsPassed,
+        markCurrentCycleAsFinished,
         setSecondsPassed,
         createNewCycle,
         interruptCurrentCycle,
