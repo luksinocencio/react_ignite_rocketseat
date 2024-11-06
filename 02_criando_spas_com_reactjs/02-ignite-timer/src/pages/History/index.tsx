d7a3696de97f7f2d3c8abcaa7f0b64d146049b54
luksinocencio <luksinocencio@gmail.com>
2024-11-05 21:32:29 -0300

    feat(ignite_timer): formatacao de data

--- 02_criando_spas_com_reactjs/02-ignite-timer/src/pages/History/index.tsx
+++ 02_criando_spas_com_reactjs/02-ignite-timer/src/pages/History/index.tsx
@@ -1,3 +1,5 @@
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale/pt-BR'
 import { useContext } from 'react'
 
 import { CyclesContext } from '../../context/CyclesContext'
@@ -27,8 +29,13 @@ export function History() {
             {cycles.map((cycle) => (
               <tr key={cycle.id}>
                 <td>{cycle.task}</td>
-                <td>{cycle.minutesAmount}</td>
-                <td>{cycle.startDate.toISOString()}</td>
+                <td>{cycle.minutesAmount} minutos</td>
+                <td>
+                  {formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </td>
                 <td>{cycle.finishedDate && <Status statusColor="green">Concluído</Status>}</td>
                 <td>{cycle.interruptedDate && <Status statusColor="red">Interrompido</Status>}</td>
                 <td>
