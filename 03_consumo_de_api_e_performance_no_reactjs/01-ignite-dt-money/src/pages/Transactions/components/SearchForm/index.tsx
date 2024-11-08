79378d77eccd04b461d710b3615e9bec630af09c
luksinocencio <luksinocencio@gmail.com>
2024-11-07 23:49:32 -0300

    feat(dt_money): add component SearchForm

---
+++ 03_consumo_de_api_e_performance_no_reactjs/01-ignite-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,14 @@
+import {SearchFormContainer} from "./styles";
+import {MagnifyingGlass} from "@phosphor-icons/react";
+
+export function SearchForm() {
+  return (
+    <SearchFormContainer>
+      <input type="text" placeholder="Busque por transações"/>
+      <button type="submit">
+        <MagnifyingGlass size={20}/>
+        Buscar
+      </button>
+    </SearchFormContainer>
+  );
+}
