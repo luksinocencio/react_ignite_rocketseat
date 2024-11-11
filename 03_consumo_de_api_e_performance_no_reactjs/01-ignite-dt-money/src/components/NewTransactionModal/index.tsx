02ca54aa3f62c1d7449cb60b534c26c22055f80c
luksinocencio <luksinocencio@gmail.com>
2024-11-11 00:11:18 -0300

    feat(dt_money): aplicando react hook form

--- 03_consumo_de_api_e_performance_no_reactjs/01-ignite-dt-money/src/components/NewTransactionModal/index.tsx
+++ 03_consumo_de_api_e_performance_no_reactjs/01-ignite-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,42 +1,80 @@
-import { ArrowCircleDown, ArrowCircleUp, X } from "@phosphor-icons/react";
-import * as Dialog from "@radix-ui/react-dialog";
-import {
-  CloseButton,
-  Content,
-  Overlay,
-  TransactionType,
-  TransactionTypeButton,
-} from "./styles";
+import {zodResolver} from '@hookform/resolvers/zod';
+import * as Dialog from '@radix-ui/react-dialog';
+import {ArrowCircleDown, ArrowCircleUp, X} from "@phosphor-icons/react";
+import {useForm} from 'react-hook-form';
+import * as z from 'zod';
+
+import {CloseButton, Content, Overlay, TransactionType, TransactionTypeButton} from './styles';
+
+const newTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  // type: z.enum(['income', 'outcome']),
+});
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
+  const {
+    register,
+    handleSubmit,
+    formState: {isSubmitting}
+  } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema),
+  })
+
+  async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(data);
+  }
+
   return (
     <Dialog.Portal>
-      <Overlay />
+      <Overlay/>
 
       <Content>
         <Dialog.Title>Nova Transação</Dialog.Title>
 
         <CloseButton>
-          <X size={24} />
+          <X size={24}/>
         </CloseButton>
 
-        <form>
-          <input type="text" placeholder="Descrição" required />
-          <input type="number" placeholder="Preço" required />
-          <input type="text" placeholder="Categoria" required />
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input
+            type="text"
+            placeholder="Descrição"
+            required
+            {...register('description')}
+          />
+          <input
+            type="number"
+            placeholder="Preço"
+            required
+            {...register('price', {valueAsNumber: true})}
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+            {...register('category')}
+          />
 
           <TransactionType>
             <TransactionTypeButton variant="income" value="income">
-              <ArrowCircleUp size={24} />
+              <ArrowCircleUp size={24}/>
               Entrada
             </TransactionTypeButton>
             <TransactionTypeButton variant="outcome" value="outcome">
-              <ArrowCircleDown size={24} />
+              <ArrowCircleDown size={24}/>
               Saída
             </TransactionTypeButton>
           </TransactionType>
 
-          <button type="submit">Cadastrar</button>
+          <button type="submit" disabled={isSubmitting}>
+            Cadastrar
+          </button>
         </form>
       </Content>
     </Dialog.Portal>
--- 03_consumo_de_api_e_performance_no_reactjs/01-ignite-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
+++ 03_consumo_de_api_e_performance_no_reactjs/01-ignite-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,34 @@
-import { MagnifyingGlass } from "@phosphor-icons/react";
-import { SearchFormContainer } from "./styles";
+import {MagnifyingGlass} from "@phosphor-icons/react";
+import {SearchFormContainer} from "./styles";
+import {useForm} from "react-hook-form";
+import * as zod from 'zod'
+import {zodResolver} from "@hookform/resolvers/zod";
+
+const searchFormSchema = zod.object({
+  query: zod.string()
+})
+
+type SearchFormInputs = zod.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const {register, handleSubmit, formState: {isSubmitting}} = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema)
+  })
+
+
+  function handleSearchTransaction(data: SearchFormInputs) {
+    console.log(data)
+  }
+
   return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
-      <button type="submit">
-        <MagnifyingGlass size={20} />
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransaction)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20}/>
         Buscar
       </button>
     </SearchFormContainer>
