6aa33cafe184bf1a1db8977b7b2c4a16d0f43360
luksinocencio <luksinocencio@gmail.com>
2024-11-14 09:53:59 -0300

    feat(pizzashop_web): setup react router DOM

--- 04_integrando_frontend_backend/01-pizzashop-web/eslint.config.js
+++ 04_integrando_frontend_backend/01-pizzashop-web/eslint.config.js
@@ -1,10 +1,10 @@
 // // eslint.config.js
-import js from '@eslint/js';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -22,16 +22,13 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'simple-import-sort/imports': 'error',
       // Espaçamento entre chaves
       'object-curly-spacing': ['error', 'always'],
       // Espaçamento entre colchetes
       'array-bracket-spacing': ['error', 'always'],
-      'semi': ['error', 'never'],
+      semi: ['error', 'never'],
     },
   },
-);
+)
