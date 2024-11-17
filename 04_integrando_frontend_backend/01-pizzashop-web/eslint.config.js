ecc151eaff98122625c46cf7470b097fae84e51b
luksinocencio <luksinocencio@gmail.com>
2024-11-17 00:57:25 -0300

    feat(pizzashop_web): add signIn Service

--- 04_integrando_frontend_backend/01-pizzashop-web/eslint.config.js
+++ 04_integrando_frontend_backend/01-pizzashop-web/eslint.config.js
@@ -26,6 +26,7 @@ export default tseslint.config(
       'simple-import-sort/imports': 'error',
       'object-curly-spacing': ['error', 'always'],
       semi: ['error', 'never'],
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 )
--- 04_integrando_frontend_backend/01-pizzashop-web/src/pages/auth/sign-in.tsx
+++ 04_integrando_frontend_backend/01-pizzashop-web/src/pages/auth/sign-in.tsx
@@ -1,12 +1,14 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
-
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
+
+import { signIn } from '@/api/sign-in'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -21,11 +23,17 @@ export function SignIn() {
     formState: { isSubmitting },
   } = useForm<SignInForm>()
 
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
+
   async function handleSignIn(data: SignInForm) {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-    console.log(data)
-    console.log(data.email)
-    toast.success('Enviamos um link de autenticação para o seu e-mail!')
+    try {
+      await authenticate(data)
+      toast.success('Login efetuado com sucesso!')
+    } catch (error) {
+      toast.error('E-mail ou senha inválidos')
+    }
   }
 
   return (
