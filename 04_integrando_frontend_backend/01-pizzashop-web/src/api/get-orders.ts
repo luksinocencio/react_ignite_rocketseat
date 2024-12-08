e1cc688227879b40c928bec0985ce78e7a60f18a
luksinocencio <luksinocencio@gmail.com>
2024-12-08 02:42:43 -0300

    feat(pizzashop_web): add listagem de pedidos

---
+++ 04_integrando_frontend_backend/01-pizzashop-web/src/api/get-orders.ts
@@ -0,0 +1,24 @@
+import { api } from '@/lib/axios.ts'
+
+interface GetOrdersResponse {
+  orders: {
+    orderId: string
+    createdAt: string
+    status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+    customerName: string
+    total: number
+  }[]
+  meta: {
+    pageIndex: number
+    perPage: number
+    totalCount: number
+  }
+}
+
+export async function getOrders() {
+  const response = await api.get<GetOrdersResponse>('/orders', {
+    params: {},
+  })
+
+  return response.data
+}
