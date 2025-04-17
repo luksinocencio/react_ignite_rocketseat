4039dbc4eca8d5461c637cf5dec457c73859d29c
luksinocencio <luksinocencio@gmail.com>
2025-04-16 21:51:57 -0300

    feat(pizzashop_web): add vitest and setup

--- 04_integrando_frontend_backend/01-pizzashop-web/vite.config.ts
+++ 04_integrando_frontend_backend/01-pizzashop-web/vite.config.ts
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
+import { InlineConfig } from 'vitest/node'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,4 +10,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
