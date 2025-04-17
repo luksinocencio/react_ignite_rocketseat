89e3f33046e4412259f1b8d2b049c77b01f26f88
luksinocencio <luksinocencio@gmail.com>
2025-04-16 22:06:36 -0300

    feat(pizzashop_web): test OrderStatus component

--- 04_integrando_frontend_backend/01-pizzashop-web/src/components/order-status.tsx
+++ 04_integrando_frontend_backend/01-pizzashop-web/src/components/order-status.tsx
@@ -9,7 +9,7 @@ interface OrderStatusProps {
   status: OrderStatus
 }
 
-const orderStatusMap = {
+const orderStatusMap: Record<OrderStatus, string> = {
   pending: 'Pendente',
   canceled: 'Cancelado',
   delivered: 'Entregue',
@@ -20,21 +20,32 @@ const orderStatusMap = {
 export function OrderStatus({ status }: OrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
-      <span className="h-2 w-2 rounded-full bg-slate-400" />
       {status === 'pending' && (
-        <span className="h-2 w-2 rounded-full bg-slate-400" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-slate-400"
+        />
       )}
 
       {status === 'canceled' && (
-        <span className="h-2 w-2 rounded-full bg-red-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-rose-500"
+        />
       )}
 
       {status === 'delivered' && (
-        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-emerald-500"
+        />
       )}
 
       {['processing', 'delivering'].includes(status) && (
-        <span className="h-2 w-2 rounded-full bg-amber-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-amber-500"
+        />
       )}
 
       <span className="font-medium text-muted-foreground">
