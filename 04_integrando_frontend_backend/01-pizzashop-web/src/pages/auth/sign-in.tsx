99348bbdefde9b48b77f1129151aa23ed78633b2
luksinocencio <luksinocencio@gmail.com>
2025-04-19 02:30:04 -0300

    feat(pizzashop_web): add sign in test e2e

--- 04_integrando_frontend_backend/01-pizzashop-web/src/pages/auth/sign-in.tsx
+++ 04_integrando_frontend_backend/01-pizzashop-web/src/pages/auth/sign-in.tsx
@@ -35,16 +35,24 @@ export function SignIn() {
 
   async function handleSignIn(data: SignInForm) {
     try {
-      await authenticate(data)
-      toast.success('Login efetuado com sucesso!')
+      await authenticate({ email: data.email })
+
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => {
+            handleSignIn(data)
+          },
+        },
+      })
     } catch (error) {
-      toast.error('E-mail ou senha inválidos')
+      toast.error('Credenciais inválidas.')
     }
   }
 
   return (
     <>
-      <Helmet title="Sign In" />
+      <Helmet title="Login" />
 
       <div className="p-8">
         <Button variant="ghost" asChild className="absolute right-8 top-8">
@@ -53,8 +61,12 @@ export function SignIn() {
 
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
-            <p className="text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do parceiro!</p>
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
           </div>
 
           <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
@@ -63,7 +75,7 @@ export function SignIn() {
               <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button className="w-full" type="submit" disabled={isSubmitting}>
+            <Button disabled={isSubmitting} className="w-full" type="submit">
               Acessar painel
             </Button>
           </form>
